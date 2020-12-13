3ca5e8a25fafdb21188e009639e7496b76294a24
frostzzone <65735427+frostzzone@users.noreply.github.com>
2020-12-13 13:33:48 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-token = '' #Put Your bots token here
+token = 'Nzg3Nzc3NTYzNTM0NTU3MjI0.X9Z5Bw.8cZ0M3xA3i4RelsZSjSAPbg8rqo' #Put Your bots token here
 
-prefix = '' #put prefix here
+prefix = '#' #put prefix here
 
-link = '' #put bot invite link here
+link = 'https://discord.com/api/oauth2/authorize?client_id=787777563534557224&permissions=8&scope=bot' #put bot invite link here
 
-ownerid = '' #put your id here
+ownerid = '712342308565024818' #put your id here
 
